0b4158c408882c9dd585efb9bc88d241bd6614ff
abhishekaga8381 <abhishekaga8381@gmail.com>
2026-05-27 11:44:26 +0530

    27052026

---
+++ 27052026/loop.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+
+    int i = 1;
+    while(i <= 10){
+        printf("%d \n", i);
+        i++;
+    }
+    printf("Thank you!\n");
+
+
+    int j = 10;
+    while(j >= 1){
+        printf("%d \n", j);
+        j--;
+    }
+    printf("Thank you!\n");
+    return 0;
+}
